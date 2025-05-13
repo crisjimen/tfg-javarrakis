c52167363793a14b837b8dcc8f098111050e694e
crisjimen <182010261+crisjimen@users.noreply.github.com>
2025-05-13 10:40:16 +0200

    Proyecto base del backend de la aplicación Javarrakis

---
+++ backend/src/main/java/com/crisjimen/javarrakis/JavarrakisApplication.java
@@ -0,0 +1,13 @@
+package com.crisjimen.javarrakis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarrakisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarrakisApplication.class, args);
+	}
+
+}
