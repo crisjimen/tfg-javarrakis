8332a5e3cf4e706e849bf7c0c3506fdc7157f015
crisjimen <182010261+crisjimen@users.noreply.github.com>
2025-05-22 17:17:15 +0200

    Se sube la base del frontend con la instalación de tailwind

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,14 @@
+import '../index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-4xl text-blue-500 font-bold">
+        Tailwind está funcionando
+      </div>
+    </>
+  )
+}
+
+export default App
