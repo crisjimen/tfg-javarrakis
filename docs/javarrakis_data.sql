31990cc294747331e8c019a3f86715207ac4aed3
crisjimen <182010261+crisjimen@users.noreply.github.com>
2025-06-03 23:05:23 +0200

    Se modifica el .sql

--- docs/javarrakis_data.sql
+++ docs/javarrakis_data.sql
@@ -68,33 +68,6 @@ INSERT INTO `reputation_level` VALUES (1,'Acólito del Desierto',0,'Apenas ha pi
 UNLOCK TABLES;
 
 --
--- Table structure for table `token`
---
-
-DROP TABLE IF EXISTS `token`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `token` (
-  `email` varchar(150) NOT NULL,
-  `user_name` varchar(50) DEFAULT NULL,
-  `token_value` varchar(255) NOT NULL,
-  `expiration` timestamp NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  PRIMARY KEY (`email`),
-  UNIQUE KEY `token_value` (`token_value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `token`
---
-
-LOCK TABLES `token` WRITE;
-/*!40000 ALTER TABLE `token` DISABLE KEYS */;
-/*!40000 ALTER TABLE `token` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user`
 --
 
@@ -123,7 +96,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (271075961173133470,'crisjimnz','cj38696@gmail.com',1,0,NULL,'2025-05-27 18:07:42','$argon2i$v=19$m=65536,t=2,p=1$aRixM7yeFS7g/CkL0NU4JQ$oe8f03qgSAvSj8n2xhNXs2AwCbAdZucH130lSH0nsRg'),(662287616309612445,'crisu2025','crisu@example.orga',1,0,NULL,'2025-05-14 07:12:49','$argon2i$v=19$m=65536,t=2,p=1$XXCEWuNsqPst6opkAArCxQ$e6pdN06cEYSwmdf0Rh5EpzqHrvgUBi00cvDHNEQ2OJs'),(1643751330611545966,'jimenezperezcristina3@gmail.com','jimenezperezcristina3@gmail.com',1,30,'2025-05-28 16:36:15','2025-05-27 18:10:48','$argon2i$v=19$m=65536,t=2,p=1$6/JqTPpJL4w/ho3c8IJ/3Q$YtZGhbLMGM3TAS0CP7mR20czW+jqLpV/NhhCM/rF+5Y'),(1858585777353673310,'crisu','crisu@example.ga',1,0,'2025-05-14 19:24:27','2025-05-14 15:21:16','$argon2i$v=19$m=65536,t=2,p=1$DWyHSMRqjHRZxt2E3UkunQ$72py1Af18PE2BFxupGNK2/4flv2YOMT6R409eArUELY'),(4510971940477226594,'crisu2025','crisu@example.com',NULL,NULL,NULL,NULL,'$argon2i$v=19$m=65536,t=2,p=1$qT836n+F7qVxSCbM7cSisg$NzF1VrT+vx3Puph42HbdgnIzfoqGD2143QatpLrT3mc'),(4701593182480319828,'crisis','crisjimnz@gmail.com',1,164,'2025-05-21 05:52:56','2025-05-21 05:50:43','$argon2i$v=19$m=65536,t=2,p=1$6Hdp5taWvz2KS2dY1gJ+Gw$gMabBPLGRZ4v2IiUwULj8T1ezWquZKflUGKAqiinHUI'),(5283166674181570676,'crisu2025','crisu@example.org',NULL,NULL,NULL,NULL,'$argon2i$v=19$m=65536,t=2,p=1$aFdc4qngXmMnA4FQThQDcg$p+uj5r2MlX5atb2yNZVdLAIvBYEeonGyPK5Wi78PLRQ'),(6189935961856035854,'crisu2025','crisu@example.es',NULL,NULL,NULL,NULL,'$argon2i$v=19$m=65536,t=2,p=1$TW8d+9Rm+2LRGBgDiOXNUw$tA8BuDJxOZ4NuTRwNBqTneBukpST1+2DYv2vaYV7A9M'),(6807065030143985339,'crisjimnz','hola@gmail.com',1,0,NULL,'2025-05-28 16:52:04','$argon2i$v=19$m=65536,t=2,p=1$u6czdbxjccf7Frmx5uR0lA$kNuvgIKhjIkbWhzuafMt6HTf4GHWGYml0dmOCJJRK0o');
+INSERT INTO `user` VALUES (271075961173133470,'crisjimnz','cj38696@gmail.com',1,0,NULL,'2025-05-27 18:07:42','$argon2i$v=19$m=65536,t=2,p=1$aRixM7yeFS7g/CkL0NU4JQ$oe8f03qgSAvSj8n2xhNXs2AwCbAdZucH130lSH0nsRg'),(1643751330611545966,'jimenezperezcristina3@gmail.com','jimenezperezcristina3@gmail.com',1,30,'2025-05-28 16:36:15','2025-05-27 18:10:48','$argon2i$v=19$m=65536,t=2,p=1$6/JqTPpJL4w/ho3c8IJ/3Q$YtZGhbLMGM3TAS0CP7mR20czW+jqLpV/NhhCM/rF+5Y'),(3865416539220166091,'prueba','prueba@gmail.com',1,45,'2025-06-03 18:54:37','2025-06-03 18:50:12','$argon2i$v=19$m=65536,t=2,p=1$ud4JQFihGYjPREb/6FHzYg$QAChTuiZvWsPTuqoNe1Hldb0Lk2jBDkACo+fQO00Ukc');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,7 +126,7 @@ CREATE TABLE `user_progress` (
 
 LOCK TABLES `user_progress` WRITE;
 /*!40000 ALTER TABLE `user_progress` DISABLE KEYS */;
-INSERT INTO `user_progress` VALUES (1643751330611545966,1,1,10,'2025-06-02 10:54:48'),(4701593182480319828,1,1,10,'2025-06-02 15:12:39'),(4701593182480319828,2,1,15,'2025-06-02 15:19:13');
+INSERT INTO `user_progress` VALUES (1643751330611545966,1,1,10,'2025-06-02 10:54:48'),(3865416539220166091,1,1,10,'2025-06-03 18:51:17'),(3865416539220166091,2,1,15,'2025-06-03 18:51:44'),(3865416539220166091,3,1,20,'2025-06-03 18:53:14');
 /*!40000 ALTER TABLE `user_progress` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -166,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-03 17:36:12
+-- Dump completed on 2025-06-03 23:04:47
